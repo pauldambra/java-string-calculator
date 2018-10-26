243ecf7345b993bf737b935a747a78f8fb32191b
pauldambra <paul.dambra@gmail.com>
2018-10-27 00:17:09 +0100

    start to handle brackets

--- src/main/java/com/dambra/paul/stringcalculator/StringCalculator.java
+++ src/main/java/com/dambra/paul/stringcalculator/StringCalculator.java
@@ -5,39 +5,45 @@ import java.util.ArrayList;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 
-class StringCalculator {
-    /**
-     * While you don't use a parallel stream the combiner will never be called and
-     * since the reduce operation here is broken if not sequential any combiner can
-     * be provided
-     * see https://stackoverflow.com/a/24308988/222163
-     */
-    private BinaryOperator<ArrayList<String>> noOpCombiner = (a, b) -> a;
+abstract class CalculationPart {
+    protected CalculationPart next;
 
-    String calculate(String input) {
-        input = input.replace(" ", "");
-        var parts = toCalculationParts(input);
-        var result = performCalculations(parts);
+    public abstract double Calculate(double total);
 
-        return stripPointZeroFromResult(result);
+    CalculationPart addNext(CalculationPart next) {
+        this.next = next;
+        return this.next;
+    }
+}
+
+class Number extends CalculationPart {
+    private final double value;
+
+    Number(String value) {
+        this.value = Double.valueOf(value);
     }
 
-    private double performCalculations(ArrayList<String> parts) {
-        var firstCalculation = parts.subList(0, 3);
-        var tail = parts.subList(3, parts.size());
+    @Override
+    public double Calculate(double total) {
+        return next.Calculate(total + value);
+    }
+}
 
-        var total = performCalculation(firstCalculation.get(1), Double.valueOf(firstCalculation.get(0)), firstCalculation.get(2));
-        for (var index = 0; index < tail.size(); index+=2) {
-            var operator = tail.get(index);
-            var next = tail.get(index + 1);
-            total = performCalculation(operator, total, next);
-        }
+class operation extends CalculationPart {
+    private final String operator;
+    private final double nextValue;
 
-        return total;
+    operation(String operator, String nextValue) {
+        this.operator = operator;
+        this.nextValue = Double.valueOf(nextValue);
     }
 
-    private double performCalculation(String operator, double total, String next) {
-        var nextValue = Double.valueOf(next);
+    @Override
+    public double Calculate(double total) {
+        return next.Calculate(performCalculation(total));
+    }
+
+    private double performCalculation(double total) {
         switch (operator) {
             case "+":
                 total += nextValue;
@@ -54,12 +60,75 @@ class StringCalculator {
         }
         return total;
     }
+}
+
+class Terminator extends CalculationPart {
+
+    @Override
+    public double Calculate(double total) {
+        return total;
+    }
+}
+
+class StringCalculator {
+    /**
+     * While you don't use a parallel stream the combiner will never be called and
+     * since the reduce operation here is broken if not sequential any combiner can
+     * be provided
+     * see https://stackoverflow.com/a/24308988/222163
+     */
+    private BinaryOperator<ArrayList<String>> noOpCombiner = (a, b) -> a;
+
+    String calculate(String input) {
+        input = input.replaceAll("\\s+", "");
+        var parts = splitParts(input);
+        parts = collapseBrackets(parts);
+        var operations = toOperations(parts);
+        var result = operations.Calculate(0);
+
+        return stripPointZeroFromResult(result);
+    }
+
+    private ArrayList<String> collapseBrackets(ArrayList<String> parts) {
+        if (parts.contains("(")) {
+            var start = parts.indexOf("(");
+            var end = parts.indexOf(")");
+            var insideBrackets = String.join("", parts.subList(start + 1, end));
+            var result = new StringCalculator().calculate(insideBrackets);
+            var x = new ArrayList<String>();
+            for (var i = 0; i < parts.size(); i++) {
+                if (i < start || end < i) {
+                    x.add(parts.get(i));
+                }
+                if (i == end) {
+                    x.add(result);
+                }
+            }
+            parts = x;
+        }
+        return parts;
+    }
+
+    private CalculationPart toOperations(ArrayList<String> parts) {
+
+        var firstOperation = new Number(parts.get(0));
+        CalculationPart mostRecentOperation = firstOperation;
+        for (var index = 1; index < parts.size(); index+=2) {
+            var operator = parts.get(index);
+            var next = parts.get(index + 1);
+            var operation = new operation(operator, next);
+            mostRecentOperation = mostRecentOperation.addNext(operation);
+        }
+
+        mostRecentOperation.addNext(new Terminator());
+        return firstOperation;
+    }
 
     private String stripPointZeroFromResult(double result) {
         return String.valueOf(result).replaceFirst("\\.0$", "");
     }
 
-    private ArrayList<String> toCalculationParts(String input) {
+    private ArrayList<String> splitParts(String input) {
         ArrayList<String> startingParts = new ArrayList<>();
         startingParts.add("");
 
@@ -81,13 +150,17 @@ class StringCalculator {
 
     // ugh, side effects
     private void accumulateADigit(ArrayList<String> acc, Character c) {
-        var current = acc.get(acc.size() - 1) + c;
+        var last = acc.get(acc.size() - 1);
+        if (last.matches("[^0-9]")) {
+            acc.add("");
+            last = acc.get(acc.size() - 1);
+        }
+        var current = last + c;
         acc.set(acc.size() - 1, current);
     }
 
     //ugh, side effects
     private void foundOperator(ArrayList<String> acc, Character c) {
         acc.add(String.valueOf(c));
-        acc.add("");
     }
 }
