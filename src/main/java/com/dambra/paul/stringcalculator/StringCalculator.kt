9b268c9ea3fdfb454acf2bba320d57433e629a52
pauldambra <paul.dambra@gmail.com>
2018-10-27 21:59:19 +0100

    super fun refactoring

--- src/main/java/com/dambra/paul/stringcalculator/StringCalculator.kt
+++ src/main/java/com/dambra/paul/stringcalculator/StringCalculator.kt
@@ -2,13 +2,12 @@ package com.dambra.paul.stringcalculator
 
 internal class StringCalculator {
 
-    fun calculate(input: String): String {
-        return input
-                .splitToStringOperations()
-                .recursivelyProcessBracketedCalculations()
-                .stringOperationsToCalculationParts()
-                .calculate(0.0)
-                .toString()
-                .removeSuffix(".0")
-    }
+    fun calculate(input: String) =
+            input
+                    .splitToStringOperations()
+                    .recursivelyProcessBracketedCalculations()
+                    .stringOperationsToCalculationParts()
+                    .calculate(0.0)
+                    .toString()
+                    .removeSuffix(".0")
 }
